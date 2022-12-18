8528f472b94a65634db72a58486d1bc350d19a02
kaleeswaran <kalesmca@gmail.com>
2022-12-18 10:25:55 +0530

    styles added

--- nms-marathon-app/src/layout/header.js
+++ nms-marathon-app/src/layout/header.js
@@ -10,18 +10,39 @@ const HeaderComponent = () => {
         };
     };
     return (
-        <div>Header Component
-            <nav className="primary-nav">
-                {/* <Link to="/">Home</Link> */}
-                {/* <Link to="/about">About</Link> */}
+        <div className="header-container">
+            <div class="header">
+                <h1>NMS SPORTS CLUB</h1>
+                <p>உலகிற்கே உணவு கொடுக்கும் உன்னதப் பணி செய்பவனே விவசாயி.</p>
+            </div>
+
+            <div class="navbar">
+                {/* <a href="#" class="active">Home</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a> */}
                 <NavLink style={navLinkStyles} to="/dashboard">
                     Dashboard
                 </NavLink>
                 <NavLink style={navLinkStyles} to="/users-update">
-                    User-Action
-                </NavLink>
-            </nav>
+                     User-Action
+                 </NavLink>
+                <a href="#" class="right">Link</a>
+            </div>
+
         </div>
+
+
+        // <div>Header Component
+        //     <nav className="primary-nav">
+
+        //         <NavLink style={navLinkStyles} to="/dashboard">
+        //             Dashboard
+        //         </NavLink>
+        //         <NavLink style={navLinkStyles} to="/users-update">
+        //             User-Action
+        //         </NavLink>
+        //     </nav>
+        // </div>
     )
 }
 
