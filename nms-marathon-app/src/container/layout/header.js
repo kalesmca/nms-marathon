ddbc95f30d7be2da2069c5755b27c7a551d03a81
kaleeswaran <kalesmca@gmail.com>
2024-01-24 18:45:53 +0530

    nav updated as dropdown

--- nms-marathon-app/src/container/layout/header.js
+++ nms-marathon-app/src/container/layout/header.js
@@ -9,10 +9,13 @@ import { setAuthStatus } from '../../redux/actions/players';
 import { global } from "../../config/locale";
 import logoutIcon from "../../assets/power-off-solid.svg"
 import signout from '../../assets/signout.svg'
+import Dropdown from 'react-bootstrap/Dropdown';
+
 const HeaderComponent = () => {
     const playerState = useSelector((state) => state?.players)
     const dispatch = useDispatch()
     const navigate = useNavigate();
+    const [selectedNav, setSelectedNav] = useState("Dashboard")
 
     const [logOutFlag, setLogOutFlag] = useState(false);
 
@@ -43,46 +46,68 @@ const HeaderComponent = () => {
                 playerState?.authStatus === "ADMIN_ACCESS" || playerState?.authStatus === "SUPER_ADMIN_ACCESS" ? (
                     // true? (
 
-                    <div className="nav-links">
+                    <div>
+                        <Dropdown className="d-inline mx-2" value={selectedNav} >
+                                <Dropdown.Toggle id="dropdown-autoclose-true">
+                                    {selectedNav}
+                                </Dropdown.Toggle>
 
-                        <nav>
+                                <Dropdown.Menu>
+                                   
+                                <Dropdown.Item key={"1d"} value={"Dashboard"} onClick={(e) => { setSelectedNav("Dashboard") }}><Link to="/authed/dashboard">Dashboard</Link></Dropdown.Item>
+                                <Dropdown.Item  key={"2d"} value={"Player List"} onClick={(e) => { setSelectedNav("Player List") }}><Link to="/authed/player-list">Player List</Link></Dropdown.Item>
+                                <Dropdown.Item  key={"3d"} value={"Registration"} onClick={(e) => { setSelectedNav("Registration") }}><Link to="/authed/registration">Registration</Link></Dropdown.Item>
+                                <Dropdown.Item  key={"4d"} value={"T-shirt"} onClick={(e) => { setSelectedNav("T-shirt") }}><Link to="/authed/tshirt">T-shirt</Link></Dropdown.Item>
+                                <Dropdown.Item  key={"5d"} value={"Source"} onClick={(e) => { setSelectedNav("Source") }}><Link to="/authed/source">Source</Link></Dropdown.Item>
+                                {/* <Dropdown.Item  value={"Dashboard"} onClick={(e) => { setSelectedNav("Dashboard") }}>{"Dashboard"}</Dropdown.Item> */}
 
-                            <div className='link'>
-                                {/* <i className="fas fa-home" onClick={() => { navigation("/dashboard") }}></i> */}
-                                <Link to="/authed/dashboard">Dashboard</Link>
-                            </div>
-                            <div className='link'>
-                                {/* <i className="fas fa-address-card" onClick={() => { navigation("/member-list") }}></i> */}
-                                <Link to="/authed/player-list">Player List</Link>
-                            </div>
-                            <div className='link'>
-                                {/* <i className="fas fa-chart-line" onClick={() => { navigation("/member-info") }}></i> */}
-                                <Link to="/authed/registration">Registration</Link>
-                            </div>
-                            <div className='link'>
-                                {/* <i className="fas fa-chart-line" onClick={() => { navigation("/member-info") }}></i> */}
-                                <Link to="/authed/tshirt">T-shirt</Link>
-                            </div>
-                            <div className='link'>
-                                {/* <i className="fas fa-chart-line" onClick={() => { navigation("/member-info") }}></i> */}
-                                <Link to="/authed/source">Source</Link>
-                            </div>
+                                      
 
-
-                        </nav>
-
-
-
+                                    
+                                </Dropdown.Menu>
+                            </Dropdown>
                     </div>
+
+                    // <div className="nav-links">
+
+                    //     <nav>
+
+                    //         <div className='link'>
+                    //             {/* <i className="fas fa-home" onClick={() => { navigation("/dashboard") }}></i> */}
+                    //             <Link to="/authed/dashboard">Dashboard</Link>
+                    //         </div>
+                    //         <div className='link'>
+                    //             {/* <i className="fas fa-address-card" onClick={() => { navigation("/member-list") }}></i> */}
+                    //             <Link to="/authed/player-list">Player List</Link>
+                    //         </div>
+                    //         <div className='link'>
+                    //             {/* <i className="fas fa-chart-line" onClick={() => { navigation("/member-info") }}></i> */}
+                    //             <Link to="/authed/registration">Registration</Link>
+                    //         </div>
+                    //         <div className='link'>
+                    //             {/* <i className="fas fa-chart-line" onClick={() => { navigation("/member-info") }}></i> */}
+                    //             <Link to="/authed/tshirt">T-shirt</Link>
+                    //         </div>
+                    //         <div className='link'>
+                    //             {/* <i className="fas fa-chart-line" onClick={() => { navigation("/member-info") }}></i> */}
+                    //             <Link to="/authed/source">Source</Link>
+                    //         </div>
+
+
+                    //     </nav>
+
+
+
+                    // </div>
                 ) : ""
             }
             {
-                logOutFlag || playerState.authStatus != AUTH_STATUS.PENDING ? (<div onClick={() => { logout() }} className="logout-container">
+                logOutFlag || playerState.authStatus != AUTH_STATUS.PENDING ? (<div className="logout-container">
                     
-                    <span>
+                    <span onClick={() => { logout() }} >
                     <a className="logout" >Log-out</a>
                     </span>
-                    <span>
+                    <span onClick={() => { logout() }} >
                          <img src={logoutIcon} alt="SVG Image"></img>
                     </span>
                     
